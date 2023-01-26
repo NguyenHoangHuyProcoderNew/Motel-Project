f4c52dce8bbbdb4692f63d1c04ed4af680125e4c
Nguyễn Hoàng Huy <93108281+NguyenHoangHuyProcoderNew@users.noreply.github.com>
2023-01-26 22:01:02 +0700

    Update 26-01-2023 By HUY-IT

--- main.py
+++ main.py
@@ -2,132 +2,48 @@ import sqlite3
 from datetime import datetime
 from prettytable import PrettyTable
 
-def add_room(): #Hàm chức năng thêm phòng
-    room_number = input("Mời bạn nhập số phòng ( lưu ý: chỉ được chọn các phòng: 02, 03, 04, 07, 08, 09): ")
+# Kết nối đến cơ sở dữ liệu & tạo bảng cho cơ sở dữ liệu
+conn = sqlite3.connect("hotel.db")
+c = conn.cursor()
+if not c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='rooms'").fetchone():
+    c.execute("CREATE TABLE rooms (room_number TEXT, check_in_time TIMESTAMP, room_type TEXT, use_water TEXT, water_type TEXT, water_quantity INTEGER)")
+conn.commit()
+conn.close()
+
+# Hàm này để thêm phòng
+def add_room():
+    room_number = input("Mời bạn chọn phòng: ")
     while room_number not in ["02", "03", "04", "07", "08", "09"]:
-        print("Xin lỗi, số phòng bạn nhập không hợp lệ, vui lòng nhập lại!")
-        room_number = input("Mời bạn nhập số phòng ( lưu ý: chỉ được chọn các phòng: 02, 03, 04, 07, 08, 09): ")
-    check_in_time = input("Mời bạn nhập giờ hiện tại ( định dạng: giờ:phút, ví dụ: 01:00 ): ")
+        print("Xin lỗi, phòng bạn chọn hiện đang có khách hoặc không hợp lệ vui lòng chọn lại nếu vẫn không được hãy chọn phòng khác ╯︵╰")       
+        room_number = input("Mời bạn chọn lại phòng ( lưu ý: chỉ được chọn các phòng: 02, 03, 04, 07, 08, 09): ")
+    check_in_time = input("Hãy cho tôi biết giờ nhận phòng: ")
     while not is_valid_time(check_in_time):
-        print("Xin lỗi, bạn đã nhập sai định dạng của chức năng này, vui lòng nhập lại!")
-        check_in_time = input("Mời bạn nhập giờ hiện tại ( định dạng: giờ:phút, ví dụ: 01:00 ): ")
-    room_type = input("Mời bạn nhập loại phòng ( lưu ý: chỉ được nhập ML hoặc Q tức là Máy Lạnh và Quạt ): ")
+        print("Xin lỗi, bạn đã cung cấp cho tôi giờ nhận phòng với định dạng giờ hoặc một dữ liệu không hợp lệ ╯︵╰")
+        check_in_time = input("Mời bạn cung cấp lại giờ nhận phòng: ")
+    room_type = input("Mời bạn chọn loại phòng: ")
     while room_type not in ["ML", "Q"]:
-        print("Xin lỗi, loại phòng bạn nhập không hợp lệ, vui lòng nhập lại loại phòng!")
-        room_type = input("Mời bạn nhập loại phòng ( lưu ý: chỉ được nhập ML hoặc Q tức là Máy Lạnh và Quạt ): ")
-    conn = sqlite3.connect("hotel_db.sql")
+        print("Loại phòng bạn chọn không hợp lệ ╯︵╰")
+        room_type = input("Vui lòng chọn lại loại phòng ( lưu ý: bạn chỉ có thể chọn loại phòng là ML hoặc Q tương đương với phòng Quạt hoặc Máy Lạnh): ")
+    use_water = input("Phòng này có sử dụng nước uống không? Nếu có thì nhập Y còn nếu không thì nhập N: ")
+    while use_water not in ["Y", "N"]:
+        print("Xin lỗi bạn chỉ có thể chọn Y hoặc N và phải viết hoa bạn nhé ( ╹▽╹ )")
+        use_water = input("Mời bạn nhập lại lựa chọn của mình: ")
+    if use_water == "Y":
+        water_type = input("Mời bạn chọn loại nước: ")
+        while water_type not in ["NN", "NS"]:
+            print("Loại nước bạn đã chọn không hợp lệ, vui lòng chọn lại loại nước ( lưu ý: bạn chỉ có thể chọn NN hoặc NS tương đương với Nước Ngọt hoặc Nước Suối bạn nhé ( ╹▽╹ ) ")
+            water_type = input("Mời bạn chọn lại loại nước: ")
+        water_quantity = input("Bạn muốn sử dụng bao nhiêu? Hãy cho tôi biết số lượng: ")
+    else:
+        water_type = None
+        water_quantity = None
+    conn = sqlite3.connect("hotel.db")
     c = conn.cursor()
-    c.execute("INSERT INTO rooms (room_number, check_in_time, room_type) VALUES (?, ?, ?)", (room_number, check_in_time, room_type))
+    c.execute("INSERT INTO rooms (room_number, check_in_time, room_type, use_water, water_type, water_quantity) VALUES (?,?,?,?,?,?)", (room_number, check_in_time, room_type, use_water, water_type, water_quantity))
     conn.commit()
-    print("Thêm phòng thành công! Chúc bạn một ngày tốt lành ^-^")
+    print("Thêm phòng thành công! Chúc bạn một ngày tốt lành ^-^")    
     conn.close()
-
-def update_room_info(): #Hàm chức năng cập nhật thông tin phòng
-    room_number = input("Mời bạn nhập số phòng cần cập nhật thông tin ( lưu ý: chỉ được chọn các phòng: 02, 03, 04, 07, 08, 09): ")
-    conn = sqlite3.connect("hotel_db.sql")
-    c = conn.cursor()
-    c.execute("SELECT * FROM rooms WHERE room_number = ?", (room_number,))
-    room = c.fetchone()
-    if room:
-        check_in_time = input("Mời bạn nhập giờ nhận phòng mới ( định dạng: giờ:phút, ví dụ: 01:00 ): ")
-        while not is_valid_time(check_in_time):
-            print("Xin lỗi, bạn đã nhập sai định dạng của chức năng này, vui lòng nhập lại!")
-            check_in_time = input("Mời bạn nhập giờ nhận phòng mới: ")
-        room_type = input("Mời bạn nhập loại phòng mới: ")
-        while room_type not in ["ML", "Q"]:
-            print("Xin lỗi, bạn đã nhập sai loại phòng, vui lòng nhập lại!")
-            room_type = input("Mời bạn nhập loại phòng mới: ")
-        c.execute("UPDATE rooms SET check_in_time = ?, room_type = ? WHERE room_number = ?", (check_in_time, room_type, room_number))
-        conn.commit()
-        print("Chúc mừng bạn đã cập nhật thông tin phòng thành công, chúc bạn một ngày vui vẻ ^-^")
-    else:
-        print("Xin lỗi, phòng này chưa tồn tại trên hệ thống, vui lòng kiểm tra lại ^_^")
-    conn.close()
-
-def calculate_room_rate(): #Hàm này để tính tiền phòng
-    room_number = input("Bạn muốn tính tiền phòng số mấy? ( lưu ý: chỉ được chọn các phòng: 02, 03, 04, 07, 08, 09) ")
-    check_out_time = input("Mời bạn nhập giờ hiện tại: ")
-    if is_valid_time(check_out_time):
-        conn = sqlite3.connect("hotel_db.sql")
-        c = conn.cursor()
-        c.execute("SELECT check_in_time, room_type FROM rooms WHERE room_number = ?", (room_number,))
-        room = c.fetchone()
-        if room:
-            check_in_time, room_type = room
-            check_in_time = datetime.strptime(check_in_time, "%H:%M")
-            check_out_time = datetime.strptime(check_out_time, "%H:%M")
-            duration = (check_out_time - check_in_time).seconds / 60
-            if room_type == "Q":
-                if duration > 0 and duration <= 60:
-                    rate = 70
-                elif duration > 60 and duration <= 120:
-                    rate = 80
-                elif duration > 120 and duration <= 180:
-                    rate = 90
-                elif duration > 180 and duration <= 240:
-                    rate = 110
-                elif duration > 240 and duration <= 300:
-                    rate = 120
-                elif duration > 300 and duration <= 360:
-                    rate = 140
-                else:
-                    rate = 0
-            elif room_type == "ML":
-                if duration > 0 and duration <= 60:
-                    rate = 90
-                elif duration > 60 and duration <= 120:
-                    rate = 100
-                elif duration > 120 and duration <= 180:
-                    rate = 110
-                elif duration > 180 and duration <= 240:
-                    rate = 120
-                elif duration > 240 and duration <= 300:
-                    rate = 140
-                elif duration > 300 and duration <= 360:
-                    rate = 160
-                else:
-                    rate = 0
-            if rate:
-                print(f"Tiền phòng của phòng này là {rate}")
-                c.execute("DELETE FROM rooms WHERE room_number = ?", (room_number,))
-                conn.commit()
-                print("Chúc bạn một ngày tốt lành ^-^")
-            else:
-                print("Giờ trả phòng mà bạn đã nhập không hợp lệ, giờ trả phòng phải lớn hơn thời gian nhận phòng")
-        else:
-            print("Số phòng mà bạn đã nhập không hợp lệ ( lưu ý: chỉ được chọn các phòng: 02, 03, 04, 07, 08, 09)")
-        conn.close()
-    else:
-        print("Giờ trả phòng mà bạn đã nhập không hợp lệ ( nhập giờ theo định dạng: giờ:phút, ví dụ: 01:00 )")
-
-def view_all_rooms(): #Hàm chức năng hiển thị tất cả các phòng đã nhập vào và hiển thị theo dạng bảng
-    conn = sqlite3.connect("hotel_db.sql")
-    c = conn.cursor()
-    c.execute("SELECT * FROM rooms")
-    rooms = c.fetchall()
-    if rooms:
-        table = PrettyTable()
-        table.field_names = ["Room Number", "Check-in Time", "Room Type"]
-        for room in rooms:
-            table.add_row(room)
-        print(table)
-    else:
-        print("Hiện chưa có phòng nào được nhập vào ^-^")
-    conn.close()
-
-
-def see_availability(): #Hàm cho chức năng xem tất cả các phòng còn trống
-    conn = sqlite3.connect("hotel_db.sql")
-    c = conn.cursor()
-    c.execute("SELECT room_number FROM rooms")
-    occupied_rooms = [room[0] for room in c.fetchall()]
-    available_rooms = list(set(["02", "03", "04", "07", "08", "09"]) - set(occupied_rooms))
-    if available_rooms:
-        print(f"Các phòng trống là: {', '.join(available_rooms)}")
-    else:
-        print("Hiện không có phòng nào trống")
-    conn.close()
-
+#Hàm này để check điều kiện giờ nhận phòng cho hàm trên
 def is_valid_time(time):
     try:
         datetime.strptime(time, "%H:%M")
@@ -135,22 +51,158 @@ def is_valid_time(time):
     except ValueError:
         return False
 
-def main():
-    conn = sqlite3.connect("hotel_db.sql")
+#Hàm cập nhật thông tin của phòng
+def update_room_info():
+    room_number = input("Bạn muốn cập nhật thông tin của phòng số mấy? Hãy cho tôi biết số phòng: ")
+    conn = sqlite3.connect("hotel.db")
     c = conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS rooms (room_number TEXT PRIMARY KEY, check_in_time TEXT NOT NULL, room_type TEXT NOT NULL)")
-    conn.commit()
+    c.execute("SELECT * FROM rooms WHERE room_number = ?", (room_number,))
+    data = c.fetchone()
+    if data:
+        print("\nThông tin phòng hiện tại:")
+        print("Số phòng: ", data[0])
+        print("Giờ nhận phòng: ", data[1])
+        print("Loại phòng: ", data[2])
+        print("Sử dụng nước: ", data[3])
+        print("Loại nước: ", data[4])
+        print("Số lượng: ", data[5])
+        print("Đã Làm Mới Lại Thông Tin Phòng:", data[0])
+        print("\nMời Bạn Cung Cấp Thông Tin Mới:")
+        # Chọn phòng
+        new_room_number = input("Bạn có muốn đổi phòng không? Nếu không hãy nhập lại số phòng cũ: ")
+        while new_room_number not in ["02", "03", "04", "07", "08", "09"]:
+            print("Xin lỗi, phòng bạn chọn hiện đang có khách hoặc không hợp lệ vui lòng chọn lại nếu vẫn không được hãy chọn phòng khác ╯︵╰")
+            new_room_number = input("Mời bạn chọn lại phòng mới ( lưu ý: chỉ được chọn các phòng: 02, 03, 04, 07, 08, 09): ")
+        # Giờ nhận phòng         
+        new_check_in_time = input("Giờ nhận phòng mới: ")
+        while not is_valid_time(new_check_in_time):
+            print("Xin lỗi, bạn đã cung cấp cho tôi giờ nhận phòng với định dạng giờ hoặc một dữ liệu không hợp lệ ╯︵╰")
+            new_check_in_time = input("Mời bạn cung cấp lại giờ nhận phòng: ")
+        # Loại phòng 
+        new_room_type = input("Mời bạn chọn loại phòng mới: ")
+        while new_room_type not in ["ML", "Q"]:
+            print("Loại phòng bạn chọn không hợp lệ ╯︵╰")
+            new_room_type = input("Vui lòng chọn lại loại phòng ( lưu ý: bạn chỉ có thể chọn loại phòng là ML hoặc Q tương đương với phòng Quạt hoặc Máy Lạnh): ")
+        # Sử dụng nước    
+        new_use_water = input("Phòng này có sử dụng nước uống không? Nếu có thì nhập Y còn nếu không thì nhập N: ")
+        while new_use_water not in ["Y", "N"]:
+            print("Xin lỗi bạn chỉ có thể chọn Y hoặc N và phải viết hoa bạn nhé ( ╹▽╹ )")
+            new_use_water = input("Mời bạn nhập lại lựa chọn của mình: ")
+        if new_use_water == "Y":
+            new_water_type = input("Mời bạn chọn loại nước: ")
+            while new_water_type not in ["NN", "NS"]:
+                print("Loại nước bạn đã chọn không hợp lệ, vui lòng chọn lại loại nước ( lưu ý: bạn chỉ có thể chọn NN hoặc NS tương đương với Nước Ngọt hoặc Nước Suối bạn nhé ( ╹▽╹ ) ")
+                new_water_type = input("Mời bạn chọn lại loại nước: ")
+            new_water_quantity = input("Bạn muốn sử dụng bao nhiêu? Hãy cho tôi biết số lượng: ")
+        else:
+            new_water_type = None
+            new_water_quantity = None
+        c.execute("UPDATE rooms SET room_number = ?, check_in_time = ?, room_type = ?, use_water = ?, water_type = ?, water_quantity = ? WHERE room_number = ?", (new_room_number, new_check_in_time, new_room_type, new_use_water, new_water_type, new_water_quantity, room_number))
+        conn.commit()
+        print("Cập nhật thông tin thành công! Chúc bạn có một ngày làm việc vui vẻ ( ╹▽╹ )")
+    else:
+        print("Không tồn tại phòng này hãy kiểm tra lại bạn nhé ( ╹▽╹ )")
     conn.close()
+
+def calculate_room_rate():
+    room_number = input("Bạn muốn thanh toán phòng số mấy? Hãy cho tôi biết số phòng: ")
+    check_out_time = input("Hãy nhập giờ hiện tại: ")
+    conn = sqlite3.connect("hotel.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM rooms WHERE room_number = ?", (room_number,))
+    data = c.fetchone()
+    if data:
+        check_in_time = data[1]
+        room_type = data[2]
+        use_water = data[3]
+        water_type = data[4]
+        water_quantity = data[5]
+        check_in_time = datetime.strptime(check_in_time, "%H:%M")
+        check_out_time = datetime.strptime(check_out_time, "%H:%M")
+        duration = check_out_time - check_in_time
+        duration_minutes = duration.seconds / 60
+        # Thiết lập giá tiền
+        if room_type == "Q": # Giá tiền cho phòng quạt
+            if duration_minutes <= 60: # 1 giờ
+                room_rate = 80  # Giá tiền
+            elif duration_minutes <= 120: # 2 giờ
+                room_rate = 100 # Giá tiền
+            elif duration_minutes <= 180: # 3 giờ
+                room_rate = 100 # Giá tiền
+            elif duration_minutes <= 240: # 4 giờ
+                room_rate = 110 # Giá tiền
+            elif duration_minutes <= 300: # 5 giờ
+                room_rate = 120 # Giá tiền
+            elif duration_minutes <= 360: # 6 giờ
+                room_rate = 160 # Giá tiền
+        elif room_type == "ML": # Giá tiền cho phòng máy lạnh
+            if duration_minutes <= 60: # 1 giờ
+                room_rate = 100
+            elif duration_minutes <= 120: # 2 giờ
+                room_rate = 110
+            elif duration_minutes <= 180: # 3 giờ
+                room_rate = 130
+            elif duration_minutes <= 240: # 4 giờ
+                room_rate = 150
+            elif duration_minutes <= 300: # 5 giờ
+                room_rate = 160
+            elif duration_minutes <= 360: # 6 giờ
+                room_rate = 200
+        water_rate = 0
+        if use_water == "Y":
+            if water_type == "NN":
+                water_rate = water_quantity * 15
+            elif water_type == "NS":
+                water_rate = water_quantity * 5
+        total_rate = room_rate + water_rate
+        print("\nTiền phòng: ", room_rate)
+        print("Tiền nước: ", water_rate)
+        print("Tổng số tiền phải trả: ", total_rate)
+        #removing the room from the database
+        c.execute("DELETE FROM rooms WHERE room_number = ?", (room_number,))
+        conn.commit()
+        print("Thanh toán thành công! Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi. Xin cảm ơn ( ╹▽╹ )")
+    else:
+        print("Phòng không tồn tại, vui lòng kiểm tra lại số phòng.")
+
+#Hàm xem tất cả các phòng trên hệ thống    
+def view_all_rooms():
+    conn = sqlite3.connect("hotel.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM rooms")
+    data = c.fetchall()
+    x = PrettyTable()
+    x.field_names = ["Số phòng", "Giờ nhận phòng", "Loại phòng", "Sử dụng nước", "Loại nước", "Số lượng nước"]
+    for row in data:
+        x.add_row(row)
+    print(x)
+    conn.close()
+
+#Hàm xem tất cả các phòng còn trống
+def see_availability():
+    conn = sqlite3.connect("hotel.db")
+    c = conn.cursor()
+    c.execute("SELECT room_number FROM rooms")
+    occupied_rooms = [room[0] for room in c.fetchall()]
+    available_rooms = list(set(["02", "03", "04", "07", "08", "09"]) - set(occupied_rooms))
+    if available_rooms:
+        available_rooms.sort()
+        print(f"Các phòng trống là: {' | '.join(available_rooms)}")
+    else:
+        print("Hiện không có phòng nào trống")
+    conn.close()    
+
+def main():
     while True:
         print("\nChào Mừng Bạn Đến Với Chương Trình Quản Lý Nhà Nghỉ Được Tạo Bởi Nguyễn Hoàng Huy")
         print("Dưới Đây Là Menu Của Chương Trình")
         print("1. Thêm phòng mới")
         print("2. Cập nhật thông tin phòng")
-        print("3. Tính tiền phòng")
-        print("4. Xem tất cả các phòng đã thêm")
-        print("5. Xem tất cả các phòng còn trống")
+        print("3. Thanh toán")
+        print("4. Xem danh sách các phòng có khách")
+        print("5. Xem danh sách các phòng trống")
         print("6. Thoát chương trình")
-        choice = input("Nhập lựa chọn của bạn: ")
+        choice = input("Lựa chọn của bạn: ")
         if choice == "1":
             add_room()
         elif choice == "2":
@@ -159,14 +211,12 @@ def main():
             calculate_room_rate()
         elif choice == "4":
             view_all_rooms()
-        elif choice == "5":
-            see_availability()
         elif choice == "6":
-            print("Đang thoát chương trình...")
-            print("Cảm ơn bạn đã sử dụng chương trình của tôi! Hẹn gặp lại ^-^")
             break
+        elif choice == "5":
+            see_availability()        
         else:
-            print("Lựa chọn của bạn không hợp lệ, vui lòng nhập lại!")
+            print("Lựa chọn của bạn không hợp lệ, vui lòng chọn lại!")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
